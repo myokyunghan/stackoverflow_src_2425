fb286f7c8d2b814e18e4e0006cc6855d5f0feb71
ComputationalSociologyLab <thecomputationalsociologylab@gmail.com>
2025-12-01 20:25:07 +0900

    20251201

---
+++ constants.py
@@ -0,0 +1,35 @@
+class CONSTANTS:
+    verbose_loading = False
+    color_map_str = ["cool", "viridis"]
+    chatgpt_release_date = "2022.11.30"
+    start_date = "2021.11.30"
+    end_date = "2023.11.30"
+    monthly_timestamps = [
+        "2021.11.30", "2021.12.31", 
+        "2022.01.31", "2022.02.28", "2022.03.31", "2022.04.30", "2022.05.31", 
+        "2022.06.30", "2022.07.31", "2022.08.31", "2022.09.30", "2022.10.31", 
+        "2022.11.30", "2022.12.31", 
+        
+        "2023.01.31", "2023.02.28", "2023.03.31", "2023.04.30", "2023.05.31", 
+        "2023.06.30", "2023.07.31", "2023.08.31", "2023.09.30", "2023.10.31", 
+        "2023.11.30", "2023.12.31",
+
+        "2024.01.31", "2024.02.28", "2024.03.31", "2024.04.30", "2024.05.31", 
+        "2024.06.30", "2024.07.31", "2024.08.31", "2024.09.30", "2024.10.31", 
+        "2024.11.30", "2024.12.31"
+    ]
+    bert_monthly_data_dir = "../result/bert_based/run_id_0/data"
+    # 2021년 01 월부터의 데이터
+    bert_monthly_data_dir_2 = "../result/bert_based/run_id_2/data"
+    bert_monthly_data_dir_3 = "../result/bert_based/run_id_3/data"
+
+    tag_monthly_data_dir    = "../result/tag/run_id_0/data"
+    tag_monthly_data_dir_2  = "../result/tag/run_id_2/data"
+
+    lda_monthly_data_dir = "../result/lda/run_id_1/data"
+
+    bert_difficulty_data_dir = "../result/bert_based/difficulty_annotated/data"
+    lda_difficulty_data_dir = "../result/lda/difficulty_annotated/data"
+    tag_difficulty_data_dir = "../result/bert_based/difficulty_annotated/data"
+    
+    all_topics_list = list(range(0, 50))
