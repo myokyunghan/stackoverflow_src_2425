6dd3e4c9ffa3e02fd3c4b46cdef791bd0813a0f3
ComputationalSociologyLab <thecomputationalsociologylab@gmail.com>
2025-12-11 16:41:48 +0900

    20251211

--- lib/annotation/excel.py
+++ lib/annotation/excel.py
@@ -5,4 +5,6 @@ excel={
     'ver4':'_105',
     'ver5':'_103',
     'ver6':'_74', # 2324 new annotated dataset :2nd3rd_icr_df.csv
+    'ver7':'_snapshot2', # 2324 new annotated dataset :2nd3rd5th_icr_df.csv
+
 }
